34b41d6e7b3841ae1e35b017dbb66f5e39fdf458
benfugate <benjamin_fugate13@yahoo.com>
2021-09-15 16:06:56 -0400

    change docker_setup.py config location

--- src/docker_setup.py
+++ src/docker_setup.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-with open('/clash-tracker/config.json') as f:
+with open('/clash-tracker/src/config.json') as f:
     config = json.load(f)
 
 config["COC_USERNAME"] = os.environ.get("COC_USERNAME")
@@ -9,5 +9,5 @@ config["COC_PASSWORD"] = os.environ.get("COC_PASSWORD")
 config["COC_CLAN_TAG"] = os.environ.get("COC_CLAN_TAG")
 config["OUTPUT_DIR"] = "/var/www/html/"
 
-with open('/clash-tracker/config.json', 'w', encoding='utf-8') as f:
+with open('/clash-tracker/src/config.json', 'w', encoding='utf-8') as f:
     json.dump(config, f, ensure_ascii=False, indent=4)
