417a6034909b71a26497319ef92919819cb9d8e6
benfugate <benjamin_fugate13@yahoo.com>
2021-10-04 14:34:28 -0400

    Tell user if average stars is lifetime

--- www/index.php
+++ www/index.php
@@ -7,7 +7,7 @@
     <?php
         if (file_exists("/clash-tracker/data/json/current_war.json")) {
             $filename = "/clash-tracker/data/json/current_war.json";
-            print_r("This page includes an onging war. Data may be incomplete.");
+            print_r("This page includes an ongoing war. Data may be incomplete.");
         }
         else
             $filename = "/clash-tracker/data/json/clash.json";
@@ -16,9 +16,8 @@
             [$b['town_hall'], $b['trophies']] <=> [$a['town_hall'], $a['trophies']]
         );
         echo '<table cellpadding="1" cellspacing="1" border="1">';
+        echo '<td>' . 'TH' . '</td>';
         echo '<td>' . 'Name' . '</td>';
-        #echo '<td>' . 'Misses' . '</td>';
-        #echo '<td>' . 'Total' . '</td>';
         echo '<td>' . 'Percentage Missed' . '</td>';
         echo '<td>' . 'Average Stars' . '</td>';
         echo '<td>' . 'Rating Score' . '</td>';
@@ -27,13 +26,14 @@
             if ($item['in_clan']) {
                 if ($item['time_filtered_average_stars'])
                     $average_stars = $item['time_filtered_average_stars'];
+                elseif ($item['average_stars'])
+                    $average_stars = $item['average_stars'] . "*";
                 else
-                    $average_stars = $item['average_stars'];
+                    $average_stars = "";
 
                 echo '<tr>';
+                    echo '<td>' . $item['town_hall'] . '</td>';
                     echo '<td>' . $item['name'] . '</td>';
-                    #echo '<td>' . $item['misses'] . '</td>';
-                    #echo '<td>' . $item['total'] . '</td>';
                     echo '<td>' . round(($item['misses']/$item['total'])*100) . '%</td>';
                     echo '<td>' . $average_stars . '</td>';
                     echo '<td>' . $item['player_score'] . '</td>';
@@ -41,6 +41,7 @@
             }
         }
         echo '</table>';
+        echo "* filtered average stars data is empty, using player lifetime value<br>";
         echo basename($filename) . " was last updated: " . date ("F d Y H:i:s.", filemtime($filename));
     ?>
     </center>
