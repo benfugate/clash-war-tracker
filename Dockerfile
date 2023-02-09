f1a4e29607da55ecf78a00b0b5d05872c439bfb4
benfugate <benjamin_fugate13@yahoo.com>
2023-02-09 14:48:10 -0500

    more changes towards a flask app

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,15 @@
-FROM php:7.4-apache
+FROM python/slim
 
 RUN apt-get update && apt-get upgrade -y && \
-    apt-get install -y python3 python3-dev python3-pip cron
+    apt-get install -y cron
 
 COPY requirements.txt .
 RUN pip3 install -r requirements.txt && rm requirements.txt
-
-RUN mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
 
 COPY clash-cron /etc/cron.d/clash-cron
 RUN chmod 0644 /etc/cron.d/clash-cron && \
     crontab /etc/cron.d/clash-cron
 
-COPY www/ /var/www/html/
 COPY src/ /clash-tracker/src/
 COPY data/ /clash-tracker/data/
 RUN chmod -R 777 /clash-tracker/data/
--- app.py
+++ app.py
@@ -8,35 +8,56 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-clash_file = "/clash-tracker/data/json/clash.json"
-current_war = "/clash-tracker/data/json/current_war.json"
+clash_file = "data/json/clash.json"
+current_war_file = "data/json/current_war.json"
+war_picks_file = "data/json/war_picks.json"
+
+def load_member_info(member, order):
+    member_info = {}
+    member_info["trophies"] = member["trophies"]
+    member_info["town_hall"] = member["town_hall"]
+    member_info["name"] = member["name"]
+    member_info["percentage_missed"] = round((member["misses"] / member["total"]) * 100) if "misses" in member else ""
+    member_info["player_score"] = member["player_score"]
+    member_info["most_recent_war"] = member["most_recent_war"] if "most_recent_war" in member else ""
+    if "time_filtered_average_stars" in member:
+        member_info["average_stars"] = member["time_filtered_average_stars"]
+    else:
+        member_info["average_stars"] = member["average_stars"] if "average_stars" in member else ""
+        member_info["player_score"] = f"{member_info['player_score']}*"
+
+    return [member_info[item] for item in order]
 
 
 @app.route('/', methods=['GET'])
 def index():
-    load_file = current_war if os.path.exists(current_war) else clash_file
+    load_file = current_war_file if os.path.exists(current_war_file) else clash_file
     with open(load_file) as f:
         clash = json.load(f)
     output = []
+    order = ["trophies", "town_hall", "name", "percentage_missed", "average_stars", "player_score"]
     for member in clash:
-        if member["in_clan"]:
-            town_hall = member["town_hall"]
-            name = member["name"]
-            player_score = member["player_score"]
-            if member["time_filtered_average_stars"]:
-                average_stars = member["time_filtered_average_stars"]
-            else:
-                average_stars = member["average_stars"]
-                player_score = f"{player_score}*"
-            percentage_missed = round((member["misses"]/member["total"])*100)
-            output.append([town_hall, name, percentage_missed, average_stars, player_score])
-    # TODO sort the list by trophies
+        if clash[member]["in_clan"]:
+            output.append(load_member_info(clash[member], order))
+    output = [member[1:] for member in sorted(output,key=lambda l:l[0], reverse=True)]
     return render_template('index.html', data=output)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('war_picks.html')
+@app.route('/war_picks', methods=['GET', 'POST'])
+def war_picks():
+    in_war = []
+    not_in_war = []
+    order = ["trophies", "town_hall", "name", "percentage_missed", "average_stars", "player_score", "most_recent_war"]
+    with open(war_picks_file) as f:
+        clash = json.load(f)
+        for member in clash:
+            if clash[member]["in_war"]:
+                in_war.append(load_member_info(clash[member], order))
+            else:
+                not_in_war.append(load_member_info(clash[member], order))
+    in_war = sorted(in_war,key=lambda l:l[0])
+    not_in_war = sorted(not_in_war,key=lambda l:l[0])
+    return render_template('war_picks.html', in_war=in_war, not_in_war=not_in_war)
 
 
 if __name__ == '__main__':
