b8637f9be2272d416604dfad9561abd9d395f0d9
benfugate <benjamin_fugate13@yahoo.com>
2023-02-10 09:35:52 -0500

    reformat, mark inactive and new players

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-#!/bin/python3
-import json
+#!/usr/bin/python3
+
 import os
-import sys
-import time
-import traceback
-from flask import Flask, render_template, request
+import json
+import subprocess
+from datetime import datetime
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -20,11 +20,16 @@ def load_member_info(member, order):
     member_info["percentage_missed"] = round((member["misses"] / member["total"]) * 100) if "misses" in member else ""
     member_info["player_score"] = member["player_score"]
     member_info["most_recent_war"] = member["most_recent_war"] if "most_recent_war" in member else ""
-    if "time_filtered_average_stars" in member:
+    if member_info["most_recent_war"]:
+        member_info["most_recent_war"] = datetime.fromtimestamp(member_info["most_recent_war"]).strftime('%x')
+    if "time_filtered_average_stars" in member and member["time_filtered_average_stars"]:
         member_info["average_stars"] = member["time_filtered_average_stars"]
     else:
-        member_info["average_stars"] = member["average_stars"] if "average_stars" in member else ""
-        member_info["player_score"] = f"{member_info['player_score']}*"
+        if "average_stars" not in member:
+            member_info["average_stars"] = ""
+        else:
+            member_info["average_stars"] = "" if not member["average_stars"] else f"{member['average_stars']}*"
+            member_info["player_score"] = f"{member_info['player_score']}*"
 
     return [member_info[item] for item in order]
 
@@ -34,22 +39,28 @@ def index():
     load_file = current_war_file if os.path.exists(current_war_file) else clash_file
     with open(load_file) as f:
         clash = json.load(f)
+        modify_time = datetime.fromtimestamp(os.path.getmtime(load_file)).strftime('%c')
     output = []
     order = ["trophies", "town_hall", "name", "percentage_missed", "average_stars", "player_score"]
     for member in clash:
         if clash[member]["in_clan"]:
             output.append(load_member_info(clash[member], order))
     output = [member[1:] for member in sorted(output,key=lambda l:l[0], reverse=True)]
-    return render_template('index.html', data=output)
+    return render_template('index.html', data=output, load_file=load_file, last_modified=modify_time)
 
 
 @app.route('/war_picks', methods=['GET', 'POST'])
 def war_picks():
+    if request.method == 'POST':
+        if request.form.get('update') == 'update':
+            subprocess.run(["/usr/bin/python3", "/clash-tracker/src/get_active_players.py"])
+            return redirect("/war_picks")
     in_war = []
     not_in_war = []
     order = ["trophies", "town_hall", "name", "percentage_missed", "average_stars", "player_score", "most_recent_war"]
     with open(war_picks_file) as f:
         clash = json.load(f)
+        modify_time = datetime.fromtimestamp(os.path.getmtime(war_picks_file)).strftime('%c')
         for member in clash:
             if clash[member]["in_war"]:
                 in_war.append(load_member_info(clash[member], order))
@@ -57,7 +68,17 @@ def war_picks():
                 not_in_war.append(load_member_info(clash[member], order))
     in_war = sorted(in_war,key=lambda l:l[0])
     not_in_war = sorted(not_in_war,key=lambda l:l[0])
-    return render_template('war_picks.html', in_war=in_war, not_in_war=not_in_war)
+
+    # Replace the first index (trophies) with a simple list counter
+    number = 1
+    for i in range(len(in_war)):
+        in_war[i][0] = number
+        number += 1
+    for i in range(len(not_in_war)):
+        not_in_war[i][0] = number
+        number += 1
+
+    return render_template('war_picks.html', in_war=in_war, not_in_war=not_in_war, last_modified=modify_time)
 
 
 if __name__ == '__main__':
